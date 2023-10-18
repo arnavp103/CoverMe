b230e0569451443fe8cde5bd66bde5a30c0c8b21
arnavp103 <arnavp103@gmail.com>
2023-10-18 00:20:31 -0400

    jobs

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import { useSignInModal } from "@/components/layout/sign-in-modal";
+
+export default function Login() {
+	const { SignInModal, setShowSignInModal } = useSignInModal();
+	setShowSignInModal(true);
+	return <SignInModal />;
+}
